aaf65ac737fee85d775c71480b1adbcf7a04c8d3
Guilherme Capitão <guilherme.capitao95@gmail.com>
2020-03-27 03:27:53 -0300

    fix: correção no cadastro de incidents

--- semanaomnistack11/backend/src/routes.js
+++ semanaomnistack11/backend/src/routes.js
@@ -12,9 +12,9 @@ routes.post('/sessions', SessionController.create);
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
-routes.get('/profile',ProfileController.index);
+routes.get('/profile', ProfileController.index);
 
-routes.post('/incidents', IncidentController.index);
+routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
 
