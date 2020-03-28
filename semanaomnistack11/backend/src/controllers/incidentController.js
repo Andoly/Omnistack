6ca9e2df8262f4da711478691b7715864916b7d0
Andoly <andoly.cs@hotmail.com>
2020-03-28 03:09:20 -0300

    week11-change-x

--- semanaomnistack11/backend/src/controllers/incidentController.js
+++ semanaomnistack11/backend/src/controllers/incidentController.js
@@ -1,21 +1,21 @@
 const connection = require('../database/connection');
 
 module.exports = {
-    async index(request, response){
-        const { page = 1} = request.query;
+    async index(request, response) {
+        const { page = 1 } = request.query;
 
         const [count] = await connection('incidents').count();
 
         const incidents = await connection('incidents')
-            .join('ongs','ongs.id','=','incidents.ong_id')
+            .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
             .limit(5)
-            .offset((page - 1)*5)
+            .offset((page - 1) * 5)
             .select(['incidents.*',
-            'ongs.name',
-            'ongs.email',
-            'ongs.whatsapp',
-            'ongs.city',
-            'ongs.uf',
+                'ongs.name',
+                'ongs.email',
+                'ongs.whatsapp',
+                'ongs.city',
+                'ongs.uf',
             ]);
 
         response.header('X-Total-Count', count['count(*)']);
@@ -24,8 +24,11 @@ module.exports = {
     },
 
     async create(request, response) {
-        const { title, description, value} = request.body;
+        const { title, description, value } = request.body;
         const ong_id = request.headers.authorization;
+
+        console.log(request.body)
+        console.log(ong_id)
 
         const [id] = await connection('incidents').insert({
             title,
@@ -34,24 +37,24 @@ module.exports = {
             ong_id,
         });
 
-
+        console.log(id)
 
         return response.json({ id });
     },
 
-    async delete(request, response){
+    async delete(request, response) {
         const { id } = request.params;
         const ong_id = request.headers.authorization;
-        
+
         const incident = await connection('incidents')
             .where('id', id)
             .select('ong_id')
             .first();
-        
-        if(incident.ong_id !== ong_id){
-            return response.status(401).json({ error : 'Operation not permited.'});
+
+        if (incident.ong_id !== ong_id) {
+            return response.status(401).json({ error: 'Operation not permited.' });
         }
-        await connection('incidents').where('id',id).delete();
+        await connection('incidents').where('id', id).delete();
 
         return response.status(204).send();
     }
